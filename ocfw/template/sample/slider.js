74dc5974b311372fd8d8873801c3eda3e3b5ff73
highQ77 <highqualityliang.77@gmail.com>
2024-11-01 23:56:19 +0800

    optimize slider

--- ocfw/template/sample/slider.js
+++ ocfw/template/sample/slider.js
@@ -6,11 +6,13 @@ export let slider = {
         return `
             <div class="oc-area">
                 <div class="oc-pos-rel">
-                    <div class="sliderContent oc-h-640 oc-wp-100 oc-pointer-event-none">
-                        ${titles.map((t, idx) => `
-                                    <template class="image" data-src="${srcs[idx]}" data-class="oc-pos-abs oc-h-640 oc-wp-100 oc-center oc-middle" data-size="cover">
-                                        <div class="oc-color-txt-white oc-fs-72">${t}</div>
-                                    </template>`).join('')}
+                    <div class="oc-h-640 oc-wp-100 oc-overflow-hidden oc-pos-rel">
+                        <div class="sliderContent oc-pos-abs oc-h-640 oc-wp-100 oc-pointer-event-none" style="transition:left 1s;">
+                            ${titles.map((t, idx) => `
+                                        <template class="image" data-src="${srcs[idx]}" data-class="oc-pos-abs oc-h-640 oc-wp-100 oc-center oc-middle" data-size="cover">
+                                            <div class="oc-color-txt-white oc-fs-72">${t}</div>
+                                        </template>`).join('')}
+                        </div>
                     </div>
                     <div class="oc-pos-abs oc-wp-100 oc-pos-t-0">
                         <div class="oc-area oc-area-v">
@@ -28,12 +30,12 @@ export let slider = {
         `
     },
     js(ele) {
-
         let sliderContent = ele.getElementsByClassName('sliderContent')[0]
         let sliderDots = ele.getElementsByClassName('sliderDots')[0]
         let sliderLeft = ele.getElementsByClassName('sliderLeft')[0]
         let sliderRight = ele.getElementsByClassName('sliderRight')[0]
         let len = sliderDots.children.length;
+        let dotStyle = sliderDots.children[0].className;
         [...sliderDots.children].reverse().forEach((c, idx) => {
             c.onclick = () => {
                 index = idx;
@@ -41,6 +43,8 @@ export let slider = {
             }
         })
         let index = len - 1
+        ani(index);
+        [...sliderContent.children].reverse().forEach((c, idx) => c.style.left = `${idx * 100}%`);
         sliderRight.onclick = () => {
             index--
             if (index == -1) index = len - 1
@@ -53,8 +57,9 @@ export let slider = {
         }
         return
         function ani(idx) {
-            [...sliderContent.children].forEach(c => c.style.display = 'none')
-            sliderContent.children[idx].style.display = 'flex'
+            sliderContent.style.left = `${-100 * (len - idx - 1)}%`;
+            [...sliderDots.children].forEach(c => c.className = dotStyle)
+            sliderDots.children[len - idx - 1].className = dotStyle + ' ' + 'oc-ol-4';
         }
     }
 }
