0f8393fd3d7da685428e6e121e0bb5f5299c05ac
highQ77 <highqualityliang.77@gmail.com>
2024-11-04 11:58:27 +0800

    page tuning

--- ocfw/page/page_ui_index.js
+++ ocfw/page/page_ui_index.js
@@ -1,8 +1,15 @@
 export let page_ui_index = `
 <main style="min-height: calc(100vh - 48px);">
+    
+    <!-- tools -->
+    <div class="oc-px-56 oc-py-8 oc-color-bg-black oc-color-txt-white">
+         <span>tools -></span>&nbsp;
+        <template class="button" onclick="showClassInPage()" data-class="oc-bd-1 oc-color-bg-primary4 oc-color-txt-white oc-w-160" data-hover="oc-color-bd-yellow">page checker</template>
+    </div>
+
     <!-- title info -->
-    <div class="oc-py-80 animate-bg">
-        <div class="oc-area oc-area-limit">
+    <div class="oc-py-120 animate-bg">
+        <div class="oc-area oc-area-limit oc-middle">
             <div>
                 <h1 class="oc-fs-30 oc-color-txt-white">Open Cute Framework <b class="oc-fs-13 oc-my-8">開源實驗框架</h1>
             </div>
@@ -12,27 +19,22 @@ export let page_ui_index = `
                         outline: 2px solid yellow;
                     }
                 </style>
-                <template class="router" data-class="oc-right oc-area" data-active="activeStyle">
+                <template class="router" data-class="oc-right" data-active="activeStyle">
                     <template data-to="page_ui_features" class="button"
-                        data-class="oc-color-bg-primary4 oc-color-txt-white"
+                        data-class="oc-color-bg-primary4 oc-color-txt-white oc-mx-4"
                         data-hover="oc-bd-4 oc-color-bd-orange">features</template>
                     <template data-to="page_ui_intro" class="button"
-                        data-class="oc-color-bg-primary4 oc-color-txt-white"
+                        data-class="oc-color-bg-primary4 oc-color-txt-white oc-mx-4"
                         data-hover="oc-bd-4 oc-color-bd-orange">intro</template>
                     <!--<template data-to="page_ui_playground" class="button"
-                        data-class="oc-color-bg-primary4 oc-color-txt-white"
+                        data-class="oc-color-bg-primary4 oc-color-txt-white oc-mx-4"
                         data-hover="oc-bd-4 oc-color-bd-orange">playground</template>-->
                     <template data-to="https://github.com/highQ77/open-cute-framework" class="button"
-                        data-class="oc-color-bg-primary4 oc-color-txt-white"
+                        data-class="oc-color-bg-primary4 oc-color-txt-white oc-mx-4"
                         data-hover="oc-bd-4 oc-color-bd-orange">github</template>
                 </template>
             </div>
         </div>
-    </div>
-
-    <div class="oc-px-56 oc-py-8 oc-color-bg-black oc-color-txt-white">
-         <span>tool -></span>&nbsp;
-        <template class="button" onclick="showClassInPage()" data-class="oc-bd-1 oc-color-bg-primary4 oc-color-txt-white oc-w-160" data-hover="oc-color-bd-yellow">page classes</template>
     </div>
 
     <div id="router"></div>
