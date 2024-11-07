64f04dd5ee331d3d470a51a60774d65b78c13786
highQ77 <highqualityliang.77@gmail.com>
2024-11-07 19:44:09 +0800

    image w & h define

--- ocfw/page/tutorial.js
+++ ocfw/page/tutorial.js
@@ -1,6 +1,6 @@
 export let tutorial = `
     <div class="oc-area-limit oc-center">
-        <template class="image" data-src="./assets/imgs/tutorial.png" data-class="oc-mt-24 oc-wp-70 oc-h-640 oc-r-8 oc-op-70" data-size="full">
+        <template class="image" data-src="./assets/imgs/tutorial.png" data-class="oc-mt-24 oc-r-8" data-params="{width:889,height:700}" data-size="full">
         </template>
     </div>
 `
--- ocfw/template/sample/image.js
+++ ocfw/template/sample/image.js
@@ -4,4 +4,10 @@ export let image = {
             <div>[slot]</div>
         `
     },
+    js(ele, params) {
+        if (params) {
+            ele.style.width = params.width + 'px';
+            ele.style.height = params.height + 'px';
+        }
+    }
 }
